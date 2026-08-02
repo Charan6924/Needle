4c02f0d1f4647b3943d11ccf250d0a55d4ae3fa0
Charan6924 <altmrcharbot@gmail.com>
2026-08-02 18:42:04 +0530

    logsumexp done

--- python/needle/ops/ops_logarithmic.py
+++ python/needle/ops/ops_logarithmic.py
@@ -2,6 +2,7 @@ from typing import Optional, Any, Union
 from ..autograd import NDArray
 from ..autograd import Op, Tensor, Value, TensorOp
 from ..autograd import TensorTuple, TensorTupleOp
+import needle as ndl
 
 from .ops_mathematic import *
 
@@ -28,15 +29,30 @@ class LogSumExp(TensorOp):
         self.axes = axes
 
     def compute(self, Z: NDArray) -> NDArray:
-        ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
-        ### END YOUR SOLUTION
+        m = array_api.max(Z,axis=self.axes,keepdims=True)
+        m_broadcast = array_api.broadcast_to(m,Z.shape)
+        exp_z = array_api.exp(Z - m_broadcast)
+        exp_sum = array_api.sum(exp_z, axis=self.axes)
+        m_reduced = Z.max(axis=self.axes)
+        return array_api.log(exp_sum) + m_reduced
 
     def gradient(self, out_grad: Tensor, node: Tensor):
-        ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
-        ### END YOUR SOLUTION
+        Z = node.inputs[0]
+        m = Tensor(array_api.max(Z.cached_data, axis=self.axes, keepdims=True),
+               device=Z.device, dtype=Z.dtype, requires_grad=False)
+        exp_z = ndl.exp(Z - m.broadcast_to(Z.shape))
+        sum_exp = ndl.summation(exp_z,axes=self.axes)
 
+        new_shape = list(Z.shape)
+        axes = range(len(new_shape)) if self.axes is None else self.axes
+        for ax in axes:
+            new_shape[ax] = 1
+        sum_exp_broadcast = ndl.broadcast_to(ndl.reshape(sum_exp, tuple(new_shape)), Z.shape)
+        softmax = exp_z / sum_exp_broadcast
+
+        out_grad_broadcast = ndl.broadcast_to(ndl.reshape(out_grad, tuple(new_shape)), Z.shape)
+        return out_grad_broadcast * softmax
+        
 
 def logsumexp(a: Tensor, axes: Optional[tuple] = None) -> Tensor:
     return LogSumExp(axes=axes)(a)
