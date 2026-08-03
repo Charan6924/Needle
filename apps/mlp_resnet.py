ffd7fa12835f191f3851814dfef1ee0aac2b52e2
Charan6924 <altmrcharbot@gmail.com>
2026-08-03 18:38:36 +0530

    mlp done didnt write a dataset object

--- apps/mlp_resnet.py
+++ apps/mlp_resnet.py
@@ -12,9 +12,18 @@ np.random.seed(0)
 
 
 def ResidualBlock(dim, hidden_dim, norm=nn.BatchNorm1d, drop_prob=0.1):
-    ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
-    ### END YOUR SOLUTION
+    main_path = nn.Sequential(
+        nn.Linear(dim,hidden_dim),
+        norm,
+        nn.Relu(),
+        nn.Dropout(drop_prob),
+        nn.Linear(hidden_dim,dim),
+        norm
+    )
+    return nn.Sequential(
+        nn.Residual(main_path),
+        nn.Relu()
+    )
 
 
 def MLPResNet(
@@ -25,16 +34,45 @@ def MLPResNet(
     norm=nn.BatchNorm1d,
     drop_prob=0.1,
 ):
-    ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
-    ### END YOUR SOLUTION
+    modules = [nn.Linear(dim,hidden_dim),nn.Relu()]
+    for _ in range(num_blocks):
+        modules.append(ResidualBlock(dim,hidden_dim,norm,drop_prob))
+    modules.append(nn.Linear(hidden_dim,num_classes))
+    return nn.Sequential(*modules)
 
 
 def epoch(dataloader, model, opt=None):
     np.random.seed(4)
-    ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
-    ### END YOUR SOLUTION
+    loss_fn = nn.SoftmaxLoss()
+    if opt is not None:
+        model.train()
+    else:
+        model.eval()
+
+    total_loss = 0.0
+    total_error = 0
+    total_samples = 0
+
+    for X,y in dataloader:
+        logits = model(X)
+        loss = loss_fn(logits,y)
+
+        if opt is not None:
+            opt.reset_grad()
+            loss.backward()
+            opt.step()
+
+        batch_size = X.shape[0]
+        total_samples += batch_size
+        total_loss += loss.numpy().item() * batch_size
+
+        preds = np.argmax(logits.numpy(), axis=1)
+        total_error += np.sum(preds != y.numpy())
+
+    avg_loss = total_loss / total_samples
+    avg_error = total_error / total_samples
+    return avg_error, avg_loss
+
 
 
 def train_mnist(
